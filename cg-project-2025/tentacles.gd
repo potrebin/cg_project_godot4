c5929a7099763047729da83686da57c1f88cdbd3
Neital <natalifrant@gmail.com>
2026-01-14 17:19:23 +0200

    change tentacle behaviour

--- cg-project-2025/tentacles.gd
+++ cg-project-2025/tentacles.gd
@@ -2,7 +2,14 @@ extends Node3D
 
 @onready var anim: AnimationPlayer = $AnimationPlayer
 
+@onready var ik: SkeletonIK3D = $Armature/Skeleton3D/TentacleIK
+@onready var target: Node3D = $IKTarget
+
 func _ready():
-	randomize()
-	anim.speed_scale = randf_range(0.25, 0.65)
-	anim.play("Action")
+	#randomize()
+	#anim.speed_scale = randf_range(0.25, 0.65)
+	#anim.play("Action")
+	ik.start()
+	var target_pos = Vector3(0, -20.0, -20.0)
+	target.global_position += target_pos
+	target.look_at(target_pos, Vector3.FORWARD)
